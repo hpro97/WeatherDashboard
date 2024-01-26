1e7d8f0ed8d223b1f48847116a7ac43ab96bb59f
Hpro97 <haxmo25@gmail.com>
2024-01-26 15:56:03 +0000

    added requirements with structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,32 @@
 // ---------------------------------//
 //functionality requirements:
 
+//use local storage to store persistent data
+
+//use 5 day weather forecast api
+
+//search via city name
+
+//display search history
+    //when clicked on, access local storage and make current weather api call
+        //display to current
+        //display to 5 day forecast
+
+//display date and time
+
+//display current weather
+    //display weather icon based on weather description
+    //display temperature
+    //display humidity
+    //display wind speed
+
+//display 5 day forecast
+    //display date and time
+    //display weather icon based on weather description
+    //display temperature
+    //display humidity
+
+
 //plan of attack:
 
 // ---------------------------------//
