38103909ba4af2638c1325fb8432631a06d6dec8
Hpro97 <haxmo25@gmail.com>
2024-01-26 17:13:47 +0000

    got working dayjs, current weather when searched and appended to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,14 @@
         //display to current
         //display to 5 day forecast
 
-//display date and time
+//display date and time ✅
+    //use dayjs for id date ✅
 
 //display current weather
-    //display weather icon based on weather description
-    //display temperature
-    //display humidity
-    //display wind speed
+    //display weather icon based on weather description ✅
+    //display temperature ✅
+    //display humidity ✅
+    //display wind speed ✅
 
 //display 5 day forecast
     //display date and time
@@ -38,7 +39,10 @@
 // ---------------------------------//
 //queryselectors for all//
 // ---------------------------------//
-
+let dateEl = $("#date");
+let timeEl = $("#time");
+let todayEl = $("#today");
+let forecastEl = $("#forecast");
 
 // ---------------------------------//
 // ---------------------------------//
@@ -47,6 +51,13 @@
 //define initial variables//
 // ---------------------------------//
 
+//move above usage
+let city;
+
+//this prevents other things working in page, turn off when fault finding
+let APIKey = "447b3ffda308ccb841ddc7ea7984552e";
+let currentqueryURL;
+
 
 // ---------------------------------//
 // ---------------------------------//
@@ -54,6 +65,7 @@
 // ---------------------------------//
 //convenience variables//
 // ---------------------------------//
+let currentDay = dayjs(); //using day js library to set current day with funciton
 
 
 // ---------------------------------//
@@ -62,6 +74,53 @@
 // ---------------------------------//
 //functions i think i'll need
 // ---------------------------------//
+//set time and day to date id section
+function setDateAndTime() {
+dateEl.text(currentDay.format("dddd, MMMM D, YYYY"));
+timeEl.text(currentDay.format("h:mm A"));
+}
+
+function getWeather(){
+    currentqueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+    fetch(currentqueryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    displayCurrentWeather(data);
+    
+  });
+}
+
+function displayCurrentWeather(data) {
+    console.log(data);
+
+    $("#today").empty();
+    $("#forecast").empty();
+
+    //accessing the icon property from obj
+    let icon = data.weather[0].icon;
+    let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+    console.log("Icon URL:", iconURL);
+    
+    let temp = data.main.temp;
+    let humidity = data.main.humidity;
+    let wind = data.wind.speed;
+    let tempC = (temp - 273.15).toFixed(2);
+
+    //declare todayWeather using let
+    let todayWeather = $("<div id='todayWeather'>");
+    
+    todayWeather.append("<div id='icon'><img src=" + iconURL + "></div>")
+    todayWeather.append("<div id='temp'>Temperature: " + tempC + "°C</div>")
+    todayWeather.append("<div id='humidity'>Humidity: " + humidity + "%</div>")
+    todayWeather.append("<div id='wind'>Wind Speed: " + wind + "MPH</div>");
+
+    // Append todayWeather to the element with id 'today'
+    $("#today").append(todayWeather);
+}
 
 
 // ---------------------------------//
@@ -71,7 +130,14 @@
 //click events & local storage
 // ---------------------------------//
 
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    city = $("#search-input").val();
+    localStorage.setItem("city", city);
+    getWeather();
+});
 
 // ---------------------------------//
 // ---------------------------------//
 
+setDateAndTime();
